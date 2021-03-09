94a7700905acbb08abe5f2dffbf94000e666c8be
Adam Tyler <mr.adamtyler@gmail.com>
2021-03-09 11:46:44 -0800

    update with more routes

--- splat.py
+++ splat.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello you muppet'
+def index():
+  return 'Your face is an index page'
+
+@app.route('/monkeys')
+def get_monkeys():
+  return 'Arr here be monkeys'
+
+@app.route('/rando/<panda>')
+def rando_panda(panda):
+  return f'The URL you entered, {escape(panda)}, is silly. Do better.'
